5476a4df869eac81c58f69e43ec410e0e1331d92
Tongbin Xu <tbxu@freewheel.com>
2019-10-28 17:43:21 +0800

    fix: staging

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 var path = require('path')
 var webpack = require('webpack')
 
+const env = process.env.NODE_ENV;
+
+const isProduction = env === 'production';
+
+const entry = isProduction ? ['./index'] : [
+  'webpack-hot-middleware/client',
+  './index'
+];
+
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './index'
-  ],
+  mode: env || 'development',
+  devtool: isProduction ? null : 'cheap-module-eval-source-map',
+  entry,
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
